653d2745f0d2e10bf17a7cab8fc438ebcb26b5e2
ksoltys <krystiansoltys99@gmail.com>
2023-01-27 03:48:24 +0100

    feat: add register controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RegisterController } from './controllers/register/register.controller';
+import { RegisterService } from './services/register/register.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, RegisterController],
+  providers: [AppService, RegisterService],
 })
 export class AppModule {}
